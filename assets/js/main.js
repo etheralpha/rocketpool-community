81725f62c6a93b96b167ecb89fc0fa050c71bfd5
hanniabu <hanniabu@gmail.com>
2022-11-25 13:58:36 -0500

    add toast and notification settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,12 @@
 ---
 
 {% include js/main.js %}
-{% include js/notificationBar.js %}
+{%- if site.enableNotification == true -%}
+  {% include js/notificationBar.js %}
+{%- endif -%}
+{%- if site.enableToast == true -%}
+  {% include js/toast.js %}
+{%- endif -%}
 {% include js/lazysizes.min.js %}
 
 // Must be last due to error
